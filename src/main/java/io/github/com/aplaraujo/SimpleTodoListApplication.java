314a933f4f051c88a58d7f7356a8ecbe8d743a53
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-15 17:07:44 -0300

    first commit

---
+++ src/main/java/io/github/com/aplaraujo/SimpleTodoListApplication.java
@@ -0,0 +1,13 @@
+package io.github.com.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/com/aplaraujo/SimpleTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.aplaraujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
